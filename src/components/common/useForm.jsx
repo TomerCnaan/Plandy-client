174d09a49b0b3c58f107c62d1fca537e86063417
Tomer <cnaan.tomer@gmail.com>
2020-02-15 12:37:18 +0200

    reusable useForm hook is working

---
+++ src/components/common/useForm.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+
+// libraries
+import Joi from "joi-browser";
+
+// components
+import Input from "./input";
+import Select from "./select";
+
+const useForm = (initialState, validationSchema, doSubmit) => {
+	const [data, setData] = useState(initialState);
+	const [errors, setErrors] = useState({});
+
+	function validate() {
+		const options = { abortEarly: false };
+		const { error } = Joi.validate(data, validationSchema, options);
+		if (!error) return null;
+
+		const _errors = {};
+		for (let item of error.details) _errors[item.path[0]] = item.message;
+		return _errors;
+	}
+
+	function validateProperty({ name, value }) {
+		const obj = { [name]: value };
+		const schema = { [name]: validationSchema[name] };
+		const { error } = Joi.validate(obj, schema);
+		return error ? error.details[0].message : null;
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		const _errors = validate();
+		setErrors({ errors: _errors || {} });
+		if (_errors) return;
+
+		doSubmit();
+	}
+
+	function handleChange({ currentTarget: input }) {
+		const { name, value } = input;
+		const _errors = errors;
+		const errorMessage = validateProperty(input);
+		if (errorMessage) setErrors({ ...errors, [name]: errorMessage });
+		else setErrors({ ...errors, [name]: "" });
+
+		setData({
+			...data,
+			[name]: value
+		});
+	}
+
+	function renderButton(label) {
+		return <button disabled={validate()}>{label}</button>;
+	}
+
+	function renderInput(name, label, type = "text") {
+		return (
+			<Input
+				type={type}
+				name={name}
+				value={data[name]}
+				label={label}
+				onChange={handleChange}
+				error={errors[name]}
+			/>
+		);
+	}
+
+	// TODO: finish reusable form component
+
+	return {
+		handleSubmit,
+		renderInput,
+		renderButton
+	};
+};
+
+export default useForm;
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 // libraried
 import { Redirect } from "react-router-dom";
-import joi, { fromByteArray } from "joi-browser";
+import Joi from "joi-browser";
 
 // components
-import Form from "./common/form";
+import useForm from "./common/useForm";
 import auth from "../services/authService";
 
+const INTITIAL_STATE = {
+	email: "",
+	password: ""
+};
+
 const LoginForm = () => {
+	const schema = {
+		email: Joi.string()
+			.required()
+			.email()
+			.label("Email"),
+		password: Joi.string()
+			.required()
+			.label("Password")
+	};
+
+	const doSubmit = async () => {
+		console.log("submitted");
+	};
+
+	const { handleSubmit, renderInput, renderButton } = useForm(
+		INTITIAL_STATE,
+		schema,
+		doSubmit
+	);
+
+	if (auth.getCurrentUser()) return <Redirect to="/" />;
 	return (
 		<div>
 			<h1> Login Form!!!</h1>
+			<form onSubmit={handleSubmit} noValidate>
+				{renderInput("email", "Email")}
+				{renderInput("password", "Password", "password")}
+				{renderButton("Sign in")}
+			</form>
 		</div>
 	);
 };
